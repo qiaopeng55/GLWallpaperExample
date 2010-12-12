7e7760e884a730473c8e7abdc862219c2522332b
Mark F Guerra <markfguerra@gmail.com>
2010-12-12 18:48:18 -0500

    Draw a triangle on the screen

--- GLWallpaperExample/src/net/markguerra/android/glwallpaperexample/MyRenderer.java
+++ GLWallpaperExample/src/net/markguerra/android/glwallpaperexample/MyRenderer.java
@@ -8,9 +8,14 @@ import net.rbgrn.android.glwallpaperservice.*;
 // Original code provided by Robert Green
 // http://www.rbgrn.net/content/354-glsurfaceview-adapted-3d-live-wallpapers
 public class MyRenderer implements GLWallpaperService.Renderer {
+	GLTriangle mTriangle;
+
 	public void onDrawFrame(GL10 gl) {
 		gl.glClearColor(0.2f, 0.4f, 0.2f, 1f);
 		gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
+		
+		gl.glColor4f(.2f, 0f, .5f, 1f);
+		mTriangle.draw(gl);
 	}
 
 	public void onSurfaceChanged(GL10 gl, int width, int height) {
@@ -18,7 +23,7 @@ public class MyRenderer implements GLWallpaperService.Renderer {
 	}
 
 	public void onSurfaceCreated(GL10 gl, EGLConfig config) {
-
+		mTriangle = new GLTriangle();
 	}
 
 	/**
