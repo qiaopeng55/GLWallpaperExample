d8dc49ef7136460ce6f1bf3a9e4e6663b8454351
Mark F Guerra <markfguerra@gmail.com>
2010-12-12 19:01:57 -0500

    Fixed perspective

--- GLWallpaperExample/src/net/markguerra/android/glwallpaperexample/MyRenderer.java
+++ GLWallpaperExample/src/net/markguerra/android/glwallpaperexample/MyRenderer.java
@@ -4,6 +4,7 @@ import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
 import net.rbgrn.android.glwallpaperservice.*;
+import android.opengl.GLU;
 
 // Original code provided by Robert Green
 // http://www.rbgrn.net/content/354-glsurfaceview-adapted-3d-live-wallpapers
@@ -19,7 +20,13 @@ public class MyRenderer implements GLWallpaperService.Renderer {
 	}
 
 	public void onSurfaceChanged(GL10 gl, int width, int height) {
+		gl.glMatrixMode(GL10.GL_PROJECTION);
+		gl.glLoadIdentity();
+		GLU.gluPerspective(gl, 60f, (float)width/(float)height, 1f, 100f);
 
+		gl.glMatrixMode(GL10.GL_MODELVIEW);
+		gl.glLoadIdentity();
+		gl.glTranslatef(0, 0, -5);
 	}
 
 	public void onSurfaceCreated(GL10 gl, EGLConfig config) {
